ce085cb8bce4538a290014824bcf16e69de35ef2
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-03-19 20:52:43 +0100

    Interfacet Imælk

---
+++ src/js/Imælk.ts
@@ -0,0 +1,5 @@
+/**Interfacet Imælk skal implmenteres af de kaffedrikke som bruger mælk */
+export interface Imælk{
+    /**Angiver hvor meget mælk der skal bruges i kaffendrikken */
+    mlmælk (): number;
+}
