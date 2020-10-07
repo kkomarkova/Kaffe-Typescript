f039ca63eff0905644ac7adfc145b54bbb38a528
kkomarkova <katerina.komarkova@hotmail.com>
2020-10-07 13:57:14 +0200

    Task 7

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,21 +3,25 @@ import {Cortado} from './Cortado';
 import { Latte } from './latte';
 
 let cortado = new Cortado;
+console.log("The name of the coffee is"+ cortado.CoffeeDrink());
     console.log("The price is" + cortado.Price());
     console.log("The strength is" + cortado.Strength());
     console.log("The amount is" + cortado.Mlmilk());
 
 let latte = new Latte;
+    console.log("The name of the coffee is"+ latte.CoffeeDrink());
     console.log("The price is" + latte.Price());
     console.log("The strength is" + latte.Strength());
     console.log("The amount is" + latte.Mlmilk());
 
 let disblackcoffee = new BlackCoffee(4);
+console.log("The name of the coffee is"+ disblackcoffee.CoffeeDrink());
     console.log("The price is" + disblackcoffee.Price());
     console.log("The strength is" + disblackcoffee.Strength());
     console.log("The discount is" + disblackcoffee.discount);
 
 let blackcoffee = new BlackCoffee;
+console.log("The name of the coffee is"+ blackcoffee.CoffeeDrink());
 console.log("The price is" + blackcoffee.Price());
 console.log("The strength is" + blackcoffee.Strength());
 console.log("The discount is" + blackcoffee.discount);
